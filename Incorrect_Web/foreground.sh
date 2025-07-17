910b7fffc5dd44482281aeadf7e228abbaa556f6
omorim <masaya.omori@nttdata.com>
2025-07-17 10:08:02 +0000

    apt installで止まるのでログをdev/nullにしechoも追加

--- Incorrect_Web/foreground.sh
+++ Incorrect_Web/foreground.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Apacheをインストール
-apt update && apt install -y apache2
+apt update && apt install -y apache2 > /dev/null 2>&1
+echo OK
+
 # mod_headers を有効化
 a2enmod headers
+
 # .htaccess を有効にするために AllowOverride を変更（必要に応じてバックアップ）
 sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+
 # Cache-Control を設定する .htaccess を作成
 cat <<EOF > /var/www/html/.htaccess
 <IfModule mod_headers.c>
