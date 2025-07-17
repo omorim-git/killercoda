ce520e7e8430cdbb7648cc00dc26f5d5851f3a57
omorim <masaya.omori@nttdata.com>
2025-07-17 10:04:05 +0000

    foreground.shの空白行を削除

--- Incorrect_Web/foreground.sh
+++ Incorrect_Web/foreground.sh
@@ -2,13 +2,10 @@
 
 # Apacheをインストール
 apt update && apt install -y apache2
-
 # mod_headers を有効化
 a2enmod headers
-
 # .htaccess を有効にするために AllowOverride を変更（必要に応じてバックアップ）
 sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
-
 # Cache-Control を設定する .htaccess を作成
 cat <<EOF > /var/www/html/.htaccess
 <IfModule mod_headers.c>
@@ -17,9 +14,7 @@ cat <<EOF > /var/www/html/.htaccess
   </FilesMatch>
 </IfModule>
 EOF
-
 # サーバ起動
 systemctl restart apache2
-
 # 準備完了表示
 echo "準備完了!"
