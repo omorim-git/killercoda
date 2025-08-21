bfe9bfec7ce017d157c9c9266c2c7cd58467252b
omorim <masaya.omori@nttdata.com>
2025-08-21 10:31:20 +0000

    ファイル名修正

--- SQL_ignores_5/background.sh
+++ SQL_ignores_5/background.sh
@@ -9,7 +9,7 @@ sudo -u postgres psql -c "ALTER USER postgres_user WITH PASSWORD 'postgres_pass'
 systemctl restart postgresql
 
 PSQL_BASE=(psql -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -v ON_ERROR_STOP=1 -X -q)
-"${PSQL_BASE[@]}" -d postgres -c "CREATE DATABASE mydb;"
+"${PSQL_BASE[@]}" -d postgres -c "CREATE DATABASE data_db;"
 
 # 準備完了表示
 touch /tmp/background-finished
