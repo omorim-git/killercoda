aa68c789badaa654fbe6aeac70a9efe611539488
omorim <masaya.omori@nttdata.com>
2025-08-26 10:15:57 +0000

    .envを作成

--- Web_page_changes_automatically/foreground.sh
+++ Web_page_changes_automatically/foreground.sh
@@ -1,3 +1,4 @@
-bash --rcfile <(cat ~/.bashrc; echo 'PS1="\[\e[36m\](CUSTOM)\u@\h:\w\$ \[\e[0m\]"')
+bash --rcfile <(cat ~/.bashrc; echo 'PS1="\[\e[36m\](PRODUCTION)\u@\h:\w\$ \[\e[0m\]"')
+source .env
 while [ ! -f /tmp/background-finished ]; do echo '環境準備中のためお待ちください...'; sleep 5; done
 echo '準備完了!'
