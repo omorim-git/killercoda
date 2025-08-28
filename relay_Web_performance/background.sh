475d0f1cacdefe1e6f0fc489d35ebcf520a3e3db
omorim <masaya.omori@nttdata.com>
2025-08-28 10:16:27 +0000

    バックグラウンド実行をコメントアウト

--- relay_Web_performance/background.sh
+++ relay_Web_performance/background.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-export OWNER='omorim-git'
-export WEB_IMAGE="ghcr.io/${OWNER}/webapp-av:0.1"
-export OR_IMAGE="ghcr.io/${OWNER}/openresty-av:0.1"
+# export OWNER='omorim-git'
+# export WEB_IMAGE="ghcr.io/${OWNER}/webapp-av:0.1"
+# export OR_IMAGE="ghcr.io/${OWNER}/openresty-av:0.1"
 
-kubectl label node controlplane node-purpose=web --overwrite
-export WEB_IMAGE OR_IMAGE
-envsubst '${WEB_IMAGE} ${OR_IMAGE}' < k8s/all-av.tmpl.yaml | kubectl apply -f -
+# kubectl label node controlplane node-purpose=web --overwrite
+# export WEB_IMAGE OR_IMAGE
+# envsubst '${WEB_IMAGE} ${OR_IMAGE}' < k8s/all-av.tmpl.yaml | kubectl apply -f -
 
-kubectl -n demo rollout status deploy/clamav
-kubectl -n demo rollout status deploy/web
-kubectl -n demo rollout status deploy/reverse-proxy-av
-kubectl -n demo get pods -o wide -n demo
+# kubectl -n demo rollout status deploy/clamav
+# kubectl -n demo rollout status deploy/web
+# kubectl -n demo rollout status deploy/reverse-proxy-av
+# kubectl -n demo get pods -o wide -n demo
 
 # 準備完了表示
 touch /tmp/background-finished
